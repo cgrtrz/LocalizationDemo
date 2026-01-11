7e0e1faf8ebc6a52a388cb00823aa9249e7e0a15
cgrtrz <cagri_terzi@hotmail.com>
2026-01-11 23:46:29 +0300

    Initial Commit

---
+++ LocalizationDemo/LocalizationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizationDemoApp.swift
+//  LocalizationDemo
+//
+//  Created by Cagri Terzi on 11.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct LocalizationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
